4704f7043b79dc4f8b282e0683fbc9080331d593
Daniel Españadero <developer@daniel-espanadero.com>
2022-08-14 19:17:10 +0200

    First commit

---
+++ src/test/java/com/irondif/api/rest/ApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irondif.api.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
